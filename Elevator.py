df65c83a0bb82778e070775d37851e12a07e502d
kstrauch94 <kstrauch0694@gmail.com>
2017-02-09 18:12:07 +0100

    Updated help text

--- Elevator.py
+++ Elevator.py
@@ -11,10 +11,7 @@ if __name__ == "__main__":
     desc = "Visualizer for an Elevator domain. Configuration can be set in the VisConfig.py file or specified with arguments\n\n" \
                                                  "It loads the encoding and instance and solves it when the next action button is pressed. " \
                                                  "It then applies one action and updates the visualization to reflect that. Requests can be added at anytime.\n\n" \
-                                                 "If extra strategy encodings are used they must be included with #include directive inside the main encoding. " \
-                                                 "After solving, it checks the plan for any problems outputs any errors.\n\n" \
-                                                 "It is also possible to include more than one encoding. It creates a window for every encoding and has its file name " \
-                                                 "as window title plus an id number.\nAdditionally, instead of an encoding, a file with a plan on it can be used to" \
+                                                 ".\nAdditionally, instead of an encoding, a file with a plan on it can be used to" \
                                                  "visualize said plan.\nEncoding and instance file paths can be either given with the Config file or with arguments described below."
 
     parser = argparse.ArgumentParser(description=desc, formatter_class=RawTextHelpFormatter)
